49caafbb3e932f6da3da81c20aaf9b729e1eaf5a
Tomer <Tomer@7470Dell>
2021-08-16 00:50:02 +0300

    hi

--- SolProject3/src/application/yoyo.java
+++ SolProject3/src/application/yoyo.java
@@ -2,7 +2,7 @@ package application;
 
 public class yoyo {
  private void syso() {
-	// TODO Auto-generated method stub
+System.out.println("what the duck");	// TODO Auto-generated method stub
 
 }
 }
