cc2c2bd924dc75a3e760a6c8651cfc93b3c02149
Tomer <Tomer@7470Dell>
2021-08-16 01:17:55 +0300

    3

--- SolProject3/src/application/yoyo.java
+++ SolProject3/src/application/yoyo.java
@@ -4,6 +4,6 @@ public class yoyo {
  private void syso() {
 System.out.println("what the duck");	// TODO Auto-generated method stub
 System.out.println("what the duck222");	// TODO Auto-generated method stub
-
+System.out.println("what the duck333");	// TODO Auto-generated method stub
 }
 }
