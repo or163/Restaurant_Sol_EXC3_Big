6a1bb11a85012ad25c83d49206158ec8371c83c8
Tomer <Tomer@7470Dell>
2021-08-16 01:04:33 +0300

    last one

--- SolProject3/src/application/yoyo.java
+++ SolProject3/src/application/yoyo.java
@@ -3,6 +3,7 @@ package application;
 public class yoyo {
  private void syso() {
 System.out.println("what the duck");	// TODO Auto-generated method stub
+System.out.println("what the duck222");	// TODO Auto-generated method stub
 
 }
 }
